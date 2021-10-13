0a8e6644c864d1e458dbb2676d5ecc20992fa1bf
rutywexler <ruty80825@gmail.com>
2021-10-13 16:59:00 +0300

    initial code + Welcome function

---
+++ Targil0/Program3930.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome3930();
+            Welcome7082();
+            Console.ReadLine();
+
+
+        }
+        static partial void Welcome7082();
+        private static void welcome3930()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application");
+        }
+    }
