db486d68e2a2c15cb9ced20a8400d6e1e172db5b
tamar0239 <tamar.nus0239@gmail.com>
2021-10-15 10:45:44 +0300

    I create structs for Drone

---
+++ DAL/Drone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public Double Battery { get; set; }
+
+        }
+    }
+}
---
+++ DAL/Station.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Station
+        {
+            public int Id { get; set; }
+            public int Name { get; set; }
+            public double Longitude { get; set; }
+            public double Lattyitude { get; set; }
+            public int ChargeSlots { get; set; }
+
+        }
+    }
+}
