ab017f78f008aae004dabd420f6007f556005e55
rutywexler <ruty80825@gmail.com>
2021-10-15 10:39:54 +0300

    Ex0 Final commit

---
+++ DAL/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+        }
+    }
+}
---
+++ DAL/Parcel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Parcel
+        {
+            public int Id { get; set; }
+            public int SenderId { get; set; }
+            public int TargetId { get; set; }
+            public int Weight { get; set; }
+            public int Priority { get; set; }
+            public int Requested { get; set; }
+            public int Droneld { get; set; }
+            public int Scheduled { get; set; }
+            public int PickedUp { get; set; }
+            public int Delivered { get; set; }
+        }
+    }
+}
