75242dc746216833f3816e2ec22859d34e8806c0
YaelSlushtz <yael32323@gmail.com>
2021-10-20 14:49:56 +0300

    I opened   class DataSource

---
+++ DAL/DataSource.cs
@@ -0,0 +1,16 @@
+﻿using IDAL.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalObject
+{
+    class DataSource
+    {
+        internal static Drone[] Drones = new Drone[10];
+
+
+    }
+}
