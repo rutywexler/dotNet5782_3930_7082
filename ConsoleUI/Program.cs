2a3cc3ac9747599bced583b6739339dee3097066
tamar0239 <tamar.nus239@gmail.com>
2021-10-22 10:58:06 +0300

    I started writing the main program

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,9 +4,17 @@ namespace ConsoleUI
 {
     class Program
     {
+        public enum Options { Insert, Update, Display, ViewTheLists };
+        public enum Insert { Station, Drone, Customer, Parcel };
+        public enum Update { AssigningParcelToDrone, CollectParcel, ParcelSupply, SendingDroneforCharging, ReleasingDroneFromCharging };
+        public enum Display { BaseStation, Drone, Customer, Parcel };
+        public enum ViewTheLists { baseStations, Drone, Customers, Parcel, ParcelNotDrone, FreeBaseStations }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
+    
+    
 }
--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DalObject.DataSource;
 
 namespace DalObject
 {
     class DalObject
     {
+        public DalObject()
+        {
+            Initalize();  
+        }
     }
 }
