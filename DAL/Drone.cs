6b76807291b82b53463f720185ca26b7300be407
rutywexler <ruty80825@gmail.com>
2021-10-24 02:08:10 +0300

    override ToString functioni

--- DAL/Drone.cs
+++ DAL/Drone.cs
@@ -11,6 +11,13 @@ namespace IDAL
             public WeightCategories MaxWeight { get; set; }
             public DroneStatuses Status { get; set; }
             public Double Battery { get; set; }
+            /// <summary>
+            /// the method override ToString method
+            /// </summary>
+            public override string ToString()
+            {
+                return $"drone id: {Id} drone model: {Model}";
+            }
 
         }
     }
