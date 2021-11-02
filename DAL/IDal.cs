ae369f75d1f006fb25a42e01654ab3b484698910
rutywexler <ruty80825>
2021-11-02 18:31:12 +0200

    add using

--- DAL/IDal.cs
+++ DAL/IDal.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DalObject;
+using static DalObject.DataSource.Config;
 
 namespace DAL
 {
     interface IDal
     {
+    //    double [] PowerConsumptionRequest()
+    //    {
+     //       double[] PowerConsumption = new double[5];
+       //     PowerConsumption[1]=
+       // }
+
 
     }
 }
+                     
