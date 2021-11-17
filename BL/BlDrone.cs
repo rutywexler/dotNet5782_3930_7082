bf239ab3ab0268bdf0c3a3b6fa57d4581a099e10
tamar0239 <tamar.nus239@gmail.com>
2021-11-17 17:37:44 +0200

    I started writing BlDrone

---
+++ BL/BlDrone.cs
@@ -0,0 +1,44 @@
+﻿using BL.BO;
+using IBL;
+using IBL.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public partial class BL : IblDrone
+    {
+        public void AddDrone(int id, string model, Enums.WeightCategories MaxWeight, int stationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Drone GetDrone(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Drone> GetDrones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReleaseDroneFromCharging(int id, float timeOfCharge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendDroneForCharge(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDrone(int id, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
