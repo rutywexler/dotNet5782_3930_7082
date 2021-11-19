d6af755460c7141e7cd098ea1a932411bf6af8fd
tamar0239 <tamar.nus239@gmail.com>
2021-11-19 12:11:39 +0200

    I wrote the Idal interface

--- DAL/IDal.cs
+++ DAL/IDal.cs
@@ -11,11 +11,25 @@ namespace IDAL
 {
     public interface IDal
     {
-    //    double [] PowerConsumptionRequest()
-    //    {
-     //       double[] PowerConsumption = new double[5];
-       //     PowerConsumption[1]=
-       // }
+        public void addCustomer(int id, string phone, string name, double longitude, double latitude);
+        public void addDrone(int id, string model, WeightCategories MaxWeight);
+        public void addStation(int id, string name, double longitude, double latitude, int chargeSlots);
+        public void AssignParcelToDrone(int parcelId, int droneId);
+        public void CollectParcel(int parcelId);
+        public void SupplyParcel(int parcelId);
+        public void SendDroneToRecharge(int droneId);
+        public void ReleaseDroneFromRecharge(int droneId);
+        public Stations GetStation(int id);
+        public Drone GetDrone(int id);
+        public Customer GetCustomer(int id);
+        public Parcel GetParcel(int id);
+        public IEnumerable<Stations> GetStations();
+        public IEnumerable<Drone> GetDrones();
+        public IEnumerable<Parcel> GetParcels();
+        public IEnumerable<Customer> GetCustomers();
+        public IEnumerable<Parcel> GetUnAssignmentParcels();
+        int AvailableChargingPorts(int baseStationId);
+        public IEnumerable<Stations> GetAvailableChargingStations();
 
 
     }
