573f9e2fc24abd7ed04f09b045f2ac52d1b6d775
rutywexler <ruty80825@gmail.com>
2021-11-20 21:16:31 +0200

    worked on bl constructor

--- BL/Bl.cs
+++ BL/Bl.cs
@@ -5,17 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using BL.BO;
 using IDAL;
+using IBL;
+using static BL.BO.Enums;
 
-namespace BL
+namespace IBL
 {
-    public partial class Bl:BL
+    public partial class BL:IBL
     {
-        private IDal dal;
+        private IDal dalObject;
         private List<DroneToList> drones;
-        public Bl()
+        public BL()
         {
-            dal = new DalObject.DalObject();
+            dalObject = new DalObject.DalObject();
             drones = new List<DroneToList>();
+            initializeDrones();
+            //drones = dal.GetDrones();
+
+            
+
+        }
+        public void initializeDrones()
+        {
+            foreach (var drone in dal.GetDrones())
+            {
+                drones.Add(new DroneToList
+                {
+                    IdDrone = drone.Id,
+                    ModelDrone = drone.Model,
+                    DroneWeight = (WeightCategories)drone.MaxWeight
+                });
+            }
+
+
+
+
+            }
         }
     }
 }
