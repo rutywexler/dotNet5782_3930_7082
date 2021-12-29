3625a69b8165ac06d43aa26ee1aead83fc826e94
tamar0239 <tamar.nus239@gmail.com>
2021-12-29 14:45:17 +0200

    CONTINUE WITH MODEL

--- PL/PO/Drone.cs
+++ PL/PO/Drone.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using static BL.Enum;
+using static PL.Model.Enum;
 
 
 namespace PL.Model
