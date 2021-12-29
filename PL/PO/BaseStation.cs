eea9af1e4704c0986730aed9d5fb3a2b14a70eb9
tama239 <tamar.nus239@gmail.com>
2021-12-30 00:12:22 +0200

    finish the view

--- PL/PO/BaseStation.cs
+++ PL/PO/BaseStation.cs
@@ -1,11 +1,5 @@
-﻿using PL.Model;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace PL.Model
