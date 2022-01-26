1091f71ae8caad4b17580d0b5546e47af4f7eda0
rutywexler <ruty80825@gmail.com>
2022-01-26 17:38:01 +0200

    I started to do the xml files

---
+++ DalXml/Program.cs
@@ -0,0 +1,135 @@
+﻿using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+namespace Dal
+{
+    internal sealed class DalXml : Idal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void AddCustomer(int id, string phone, string name, double longitude, double latitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDrone(int id, string model, WeightCategories MaxWeight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDRoneCharge(int droneId, int stationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParcel(int SenderId, int TargetId, WeightCategories Weigth, Priorities Priority, int id = 0, int droneId = 0, DateTime requested = default, DateTime sceduled = default, DateTime pickedUp = default, DateTime delivered = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddStation(int id, string name, double longitude, double latitude, int chargeSlots)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AssignParcelToDrone(int parcelId, int droneId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Station> GetAvailableChargingStations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Drone GetDrone(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetDronechargingInStation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Drone> GetDrones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Parcel GetParcel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Parcel> GetParcels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Parcel> GetParcels(Predicate<Parcel> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double[] GetPowerConsumptionByDrone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Station GetStation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Station> GetStations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NotAvailableChargingPorts(int baseStationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReleaseDroneFromRecharge(int droneId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveDrone(Drone drone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveParcel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStation(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
